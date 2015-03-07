e90d46c71765a96a860f567a64b0c4887e3c62d9
Alexandre Blanquart <alex.blanquart@gmail.com>
2015-03-07 12:23:23 +0100

    blog/post, fontawesome

---
+++ static/js/app.js
@@ -0,0 +1,9 @@
+var container = document.querySelector('#mansory-container');
+var msnry;
+// initialize Masonry after all images have loaded
+imagesLoaded( container, function() {
+	msnry = new Masonry( container, {
+		// options
+	  	itemSelector: '.mansory-item'
+  	});
+});
