cdbcd912ee3b2e7648339627d002acfa3742dd69
Alexandre Blanquart <alex.blanquart@gmail.com>
2015-03-07 22:23:31 +0100

    more stuff

--- static/js/app.js
+++ static/js/app.js
@@ -8,3 +8,20 @@ imagesLoaded( container, function() {
 	  	itemSelector: '.mansory-item'
   	});
 });
+
+
+/* Carousel */
+$('#mutlicarousel .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+  
+  if (next.next().length>0) {
+    next.next().children(':first-child').clone().appendTo($(this));
+  }
+  else {
+  	$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+  }
+});
