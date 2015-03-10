df7f243b354e2c440e906f7209c9194592c98793
Alexandre Blanquart <alex.blanquart@gmail.com>
2015-03-10 23:45:02 +0100

    included fullcalendar and introducing new colors/fonts

--- static/js/app.js
+++ static/js/app.js
@@ -1,27 +1,20 @@
-/* Mansory */
-var container = document.querySelector('.mansory-container');
-var msnry;
-// initialize Masonry after all images have loaded
-imagesLoaded( container, function() {
-	msnry = new Masonry( container, {
-		// options
-	  	itemSelector: '.mansory-item'
-  	});
+$(document).ready(function() {
+
+  /* Mansory */
+  var $container = $('.mansory-container');
+  $container.imagesLoaded( function(){
+        $container.masonry({
+            itemSelector: '.mansory-item'
+        });
+    });
+
+  /* FullCalendar */
+  $('#calendar').fullCalendar({
+      height: "auto"
+  });
+
+  $('#collapseAgenda').on('shown.bs.collapse', function () {
+    $('#calendar').fullCalendar('today');
+  });
+
 });
-
-
-/* Carousel */
-/*$('#mutlicarousel .item').each(function(){
-  var next = $(this).next();
-  if (!next.length) {
-    next = $(this).siblings(':first');
-  }
-  next.children(':first-child').clone().appendTo($(this));
-  
-  if (next.next().length>0) {
-    next.next().children(':first-child').clone().appendTo($(this));
-  }
-  else {
-  	$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-  }
-});*/
